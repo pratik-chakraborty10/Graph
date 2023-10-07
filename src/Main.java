66ca533aa888563092dd4d2de9afe437722a9307
Pratik Chakraborty <118681019+pratik-chakraborty10@users.noreply.github.com>
2023-10-08 01:20:37 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 
 public class Main {
     static class Edge{
-        int source;int destination;
+        int source;
+        int destination;
 
         public Edge(int s,int d){
             this.source=s;
@@ -10,59 +11,68 @@ public class Main {
         }
     }
 
-     static void createGraph(ArrayList<Edge>graph[]){
+    static void createGraph(ArrayList<Edge>graph[]){
         for (int i=0;i< graph.length;i++){
             graph[i]=new ArrayList<>();
         }
+
+        graph[0].add(new Edge(0,1));
         graph[0].add(new Edge(0,2));
+        graph[0].add(new Edge(0,3));
 
         graph[1].add(new Edge(1,0));
+        graph[1].add(new Edge(1,2));
 
-        graph[2].add(new Edge(2,3));
+        graph[2].add(new Edge(2,0));
+        graph[2].add(new Edge(2,1));
 
         graph[3].add(new Edge(3,0));
-     }
+        graph[3].add(new Edge(3,4));
 
-     public static boolean isCycle(ArrayList<Edge>[]graph){
+        graph[4].add(new Edge(4,3));
+    }
+
+    public static boolean detectCycle(ArrayList<Edge>[]graph){
         boolean visited[]=new boolean[graph.length];
-        boolean stack[]=new boolean[graph.length];
+        for (int i=0;i< graph.length;i++){
+            if(!visited[i]){
+                if(detectCycleUtil(graph,visited,i,-1) ){
+                    return true;
+                    //cycle exists in one of the parts
+                }
+            }
+        }
+        return false;
+    }
 
-         for (int i=0;i<graph.length;i++){
-             if (!visited[i]){
-                 if(isCycleUtil( graph,i,visited,stack)){
-                     return true;
-                 }
-             }
+    public static boolean detectCycleUtil(ArrayList<Edge>[]graph,boolean visited[],int currentNode,int parentNode){
+        visited[currentNode]=true;
+        for (int i=0;i<graph[currentNode].size();i++){
+            Edge e=graph[currentNode].get(i);
+            //case->3
+            if (!visited[e.destination] ){
+                if (detectCycleUtil(graph,visited,e.destination,currentNode)){
+                    return true;
 
-         }
-         return false;
-     }
-
-     public static boolean isCycleUtil(ArrayList<Edge>[]graph,int currentnode,boolean visited[],boolean stack[]){
-         visited[currentnode]=true;
-         stack[currentnode]=true;
-
-         for (int i=0;i<graph[currentnode].size();i++){
-             Edge e=graph[currentnode].get(i);
-
-             if (stack[e.destination]){//cycle exists
-                 return true;
-             }
-             if (!visited[e.destination] && isCycleUtil(graph,e.destination,visited,stack)){
-                 return true;
+                }
 
 
-             }
-         }
-         stack[currentnode]=false;
-         return false;
-     }
+            } else if (visited[e.destination] && e.destination!=parentNode) {//case->1
+                return true;
+                
+            }
+            //case 2->do nothing->continue
+        }
+        return false;
+
+    }
     public static void main(String[] args) {
-        int vertex=4;
+
+        int vertex=5;
         ArrayList<Edge>graph[]=new ArrayList[vertex];
         createGraph(graph);
 
-        System.out.println(isCycle(graph));
+        System.out.println(detectCycle(graph));
 
     }
 }
